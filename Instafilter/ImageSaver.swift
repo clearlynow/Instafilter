14e1b4602db7b4a77a2be19e8c0bcdda5d4626c1
Alison Gorman <alison@minimascientific.com>
2022-11-15 16:13:55 -0700

    updated with challenges

---
+++ Instafilter/ImageSaver.swift
@@ -0,0 +1,25 @@
+//
+//  ImageSaver.swift
+//  Instafilter
+//
+//  Created by Alison Gorman on 11/11/22.
+//
+
+import UIKit
+
+class ImageSaver: NSObject {
+    var successHandler: (() -> Void)?
+    var errorHandler: ((Error) -> Void)?
+
+    func writeToPhotoAlbum(image: UIImage) {
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveComplete), nil)
+    }
+
+    @objc func saveComplete(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            errorHandler?(error)
+        } else {
+            successHandler?()
+        }
+    }
+}
